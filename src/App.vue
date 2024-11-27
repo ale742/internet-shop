997e58b50ca2fd2b0e456a9cd4015c8c8079576f
Алексей <leha2017tv@gmail.com>
2024-11-27 10:32:54 +0500

    feat: add lyyout

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,28 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <RouterLink to="/" class="navbar-brand">ShopFruit</RouterLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+         <li class="nav-item">
+         <RouterLink to="/" class="nav-link">Home</RouterLink>
+         </li>
+         <li class="nav-item">
+         <a class="nav-link" href="#">Cart</a>
+         </li>
+         <li class="nav-item">
+         <RouterLink to="/About" class="nav-link">About</RouterLink>
+         </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 <RouterView/>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,100 @@
 </script>
 
 <template>
-<h1>привет</h1>
+
+    <div class="row mt-4">
+        <div class="col-md-5">
+            <select class="form-select" aria-label="Default select example">
+  <option selected>Select category</option>
+  <option value="1">Vegetables</option>
+  <option value="2">fruit</option>
+  <option value="3">Berry</option>
+</select>
+         </div>
+         <div class="col-md-5">
+            <input type="email" class="form-control"  placeholder="shary be name">
+
+         </div>
+         <div class="col-md-2">
+            <button type="button" class="btn btn-outline-warning" Disabled>reset</button>
+
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-md-4  g-5 mt-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://avatars.mds.yandex.net/i?id=7b2932ef8dab2b2da9d9dae4ee1fff0bb605f350cac39e68-12740958-images-thumbs&n=13" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Banana</h5>
+        <p class="card-text">$15</p>
+        <p class="card-text">fruit</p>
+      </div>
+      <button type="button" class="btn btn-outline-info me-2">Datail</button>
+      <button type="button" class="btn btn-outline-success">cart</button>
+    </div>
+  </div>
+
+
+
+  <div class="row mt-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://avatars.mds.yandex.net/i?id=48d94e56a67714f947573d4198c7be1e_l-5303693-images-thumbs&n=13" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">apple</h5>
+        <p class="card-text">$10</p>
+        <p class="card-text">fruit</p>
+      </div> 
+      <button type="button" class="btn btn-outline-info me-2">Datail</button>
+      <button type="button" class="btn btn-outline-success">cart</button>
+</div> 
+  </div>  
+   </div>
+ 
+  <div class="row mt-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://avatars.mds.yandex.net/i?id=7fce04810ac5567adb743e9bdca0bfb98569fe5d-12475602-images-thumbs&n=13" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">маракуя</h5>
+        <p class="card-text">$20</p>
+        <p class="card-text">fruit</p>
+      </div>
+      <button type="button" class="btn btn-outline-info me-2" >Datail</button>
+      <button type="button" class="btn btn-outline-success" >cart</button>
+
+    </div>
+    </div>
+  </div>
+  <div class="row mt-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://avatars.mds.yandex.net/i?id=ca0d609e1c307cdc2f08db16feb54c3b55deea87-5316097-images-thumbs&n=13" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">kivi</h5>
+        <p class="card-text">$16</p>
+        <p class="card-text">fruit</p>
+      </div>
+      <button type="button" class="btn btn-outline-info me-2" >Datail</button>
+      <button type="button" class="btn btn-outline-success" >cart</button>
+    </div>
+  </div>
+  </div>
+  <div class="row mt-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://produktoff.kz/pictures/product/big/6933_big.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">grape</h5>
+        <p class="card-text">$10</p>
+        <p class="card-text">Berry</p>
+      </div> 
+      <button type="button" class="btn btn-outline-info me-2">Datail</button>
+      <button type="button" class="btn btn-outline-success">cart</button>
+</div> 
+  </div>  
+   </div>
+</div>
+
+
 </template>
