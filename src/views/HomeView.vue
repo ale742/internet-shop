8594d7252c772bdfc86974e3831aaeb97416c30a
Алексей <leha2017tv@gmail.com>
2024-11-27 13:42:21 +0500

    fix: home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useCategoryStore } from '@/stores/category';
+import { useCategoryStore } from '@/stores/Category';
 import { useProductStore } from '@/stores/product';
 import { ref,} from 'vue'
 
